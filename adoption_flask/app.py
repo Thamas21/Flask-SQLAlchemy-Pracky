d1ae7a39bda2dbc241c917dca9142b04f6022b49
Thomas Thelen-Clemmons <thomasthelen-clemmons@Thomass-MacBook-Air.local>
2022-03-13 16:04:55 -0700

    minor changes

--- adoption_flask/app.py
+++ adoption_flask/app.py
@@ -1,11 +1,12 @@
 from flask import (render_template,
                    url_for, request, redirect)
-from model import (db, Pet, app)
+from models import (db, Pet, app)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    pets = Pet.query.all()
+    return render_template('index.html', pets=pets)
 
 
 @app.route('/addpet', methods=['GET', 'POST'])
@@ -33,9 +34,44 @@ def add_pet():
     return render_template('addpet.html')
 
 
-@app.route('/pet')
-def pet():
-    return render_template('pet.html')
+@app.route('/pet/<id>')
+def pet(id):
+    pet = Pet.query.get_or_404(id)
+    return render_template('pet.html', pet=pet)
+
+@app.route('/editpet/<id>', methods=['GET', 'POST'])
+def editpet(id):
+    pet = Pet.query.get_or_404(id)
+    if request.form:
+        pet.name = request.form['name']
+        pet.age = request.form['age']
+        pet.breed = request.form['breed']
+        pet.url = request.form['url']
+        pet.color = request.form['color']
+        pet.size = request.form['size']
+        pet.weight = request.form['weight']
+        pet.url_tag = request.form['alt']
+        pet.pet_type = request.form['pet']
+        pet.gender = request.form['gender']
+        pet.spay = request.form['spay']
+        pet.house_train = request.form['housetrained']
+        pet.description = request.form['description']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('editpet.html', pet=pet)
+
+
+@app.route('/delete_pet/<id>')
+def delete_pet(id):
+    pet = Pet.query.get_or_404(id)
+    db.session.delete(pet)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', msg=error), 404
 
 
 if __name__ == '__main__':
--- adoption_flask/models.py
+++ adoption_flask/models.py
@@ -5,7 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pets.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
